7ac9bf37326c6d78ae19ccb8de6e864006dc19fa
loziamina02 <loziamina02@gmail.com>
2026-03-22 01:06:35 +0100

    setup backend + db config + base auth structure

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from app.database import Base, engine
+from app.models import user
+from app.routes import auth
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.include_router(auth.router, prefix="/auth", tags=["Auth"])
+
 @app.get("/")
-def read_root():
-    return {"message": "GameDash API running 🚀"}
+def root():
+    return {"message": "GameDash API running "}
---
+++ backend/app/routes/auth.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.models.user import User
+from app.schemas.user import UserCreate, UserLogin
+from app.core.security import hash_password, verify_password, create_access_token
+
+router = APIRouter()
+
+# REGISTER
+@router.post("/register")
+def register(user: UserCreate, db: Session = Depends(get_db)):
+
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already exists")
+
+    new_user = User(
+        email=user.email,
+        password=hash_password(user.password),
+        pseudo=user.pseudo
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message": "User created"}
+
+#  LOGIN
+@router.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user or not verify_password(user.password, db_user.password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    token = create_access_token({"sub": db_user.email})
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
