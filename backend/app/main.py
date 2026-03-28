18a3778f7e685219e2500c6dedcc4080ea78307b
loziamina02 <loziamina02@gmail.com>
2026-03-28 02:19:09 +0100

    feat : add login front and design

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,10 +6,19 @@ from app.routes import admin
 from app.routes import matchmaking
 from app.routes import matchmaking_ws
 from app.routes import dashboard
-
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(admin.router, prefix="/auth", tags=["Admin"])
