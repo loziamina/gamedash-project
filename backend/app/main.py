caa25daf6cf266adba47f75c76d4f558a57943e7
loziamina02 <loziamina02@gmail.com>
2026-03-28 18:33:43 +0100

    feat : ui dashboard temps réel

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,7 @@ app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(admin.router, prefix="/auth", tags=["Admin"])
 app.include_router(user.router, prefix="/users", tags=["Users"])
 app.include_router(matchmaking.router, prefix="/matchmaking", tags=["Matchmaking"])
-app.include_router(matchmaking_ws.router, tags=["Matchmaking WS"])
+app.include_router(matchmaking_ws.router)
 app.include_router(dashboard.router, prefix="/dashboard", tags=["Dashboard"])
 @app.get("/")
 def root():
